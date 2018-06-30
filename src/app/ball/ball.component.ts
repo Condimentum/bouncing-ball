00fd56bbf70cac2b19ebed916566944e9ea1db39
Axel Johansson <axel.j.johansson@gmail.com>
2018-06-30 15:42:00 +0300

    It bounces!

--- src/app/ball/ball.component.ts
+++ src/app/ball/ball.component.ts
@@ -9,35 +9,54 @@ import { BouncingBallPanelComponent } from '../bouncing-ball-panel/bouncing-ball
 export class BallComponent implements OnInit {
 
 	speed: number;
-	size: number;
+	r: number; // radius
 	color: string; // Later, enum COLOR
 	startAngle: number;
-	private positionX: number;
-	private positionY: number;
-	private angle: number;
+	private X: number;
+	private Y: number;
+	private dX: number;
+	private dY: number;
 
 
   constructor() {
-  	this.size = 20;
-  	this.positionX = 10;
-  	this.positionY = 10;
+  	this.r = 20;
+  	this.X = 200;
+  	this.Y = 200;
+  	this.dX = 1;
+  	this.dY = 1;
   }
 
-  start() {
+  draw() {
   	const bouncingBallPanelComponent = new BouncingBallPanelComponent();
   	var canvas = bouncingBallPanelComponent.getCanvas();
   	if (canvas.getContext) {
   		var ctx = canvas.getContext("2d");
+  		ctx.clearRect(0, 0, canvas.width, canvas.height);
   		ctx.beginPath();
-  		ctx.arc(50, 50, 10, 0, Math.PI*2);
-  		ctx.fillStyle = "#0095DD";
+  		ctx.arc(this.X, this.Y, this.r, 0, Math.PI*2);
+  		ctx.fillStyle = "#0095DD"; // this.color
   		ctx.fill();
   		ctx.closePath();
   	}
   }
 
+  start() {
+  	const bouncingBallPanelComponent = new BouncingBallPanelComponent();
+  	var canvas = bouncingBallPanelComponent.getCanvas();
+  	console.log(`${this.X}`, `${this.Y}`);
+  	this.draw();
+  	if (this.X + this.dX > canvas.width - this.r || this.X + this.dX < this.r) {
+  		this.dX = -this.dX;
+  	}
+  	if (this.Y + this.dY > canvas.height - this.r || this.Y + this.dY < this.r) {
+  		this.dY = -this.dY;
+  	}
+  	this.X += this.dX;
+  	this.Y += this.dY;
+  }
+
   ngOnInit() {
-  	setInterval(this.start(), 10);
+  	setInterval(() => this.start(), 10);
   }
 
 }
